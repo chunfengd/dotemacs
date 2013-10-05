7d077e6d692174e4282cab48786a6baddf58f0b6
chunfeng <chunfengd@xiaobai>
2013-10-05 02:50:09 -0400

    set up ess

--- .emacs.d/init/20-mode-ess.el
+++ .emacs.d/init/20-mode-ess.el
@@ -4,6 +4,13 @@
 
 ;;(add-to-list 'load-path "~/.emacs.d/elpa/auto-complete-1.4")
 
-;;(require 'ess-site)
+(require 'ess-site)
 
+;; ESS will not print the evaluated comands, also speeds up the
+;; evaluation
+(setq ess-eval-visibly nil)
+
+;; if you don't want to be prompted each time you start an interactive
+;; R session
+ (setq ess-ask-for-ess-directory nil)
 
