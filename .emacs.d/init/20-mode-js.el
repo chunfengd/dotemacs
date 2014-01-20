1b05d75039c5ab66cc61ab75cab6ec63f4fc2357
chunfeng <chunfengd@xiaohuihui>
2014-01-19 21:09:06 -0500

    comment out autoload js2-mode

--- .emacs.d/init/20-mode-js.el
+++ .emacs.d/init/20-mode-js.el
@@ -1,7 +1,7 @@
 
 (cf-install-packages '(js2-mode))
 
-(autoload 'js2-mode "js2" nil t)
+;; (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (defun cf-init-js ()
