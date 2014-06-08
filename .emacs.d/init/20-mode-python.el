6e55f7f9c38ec0b34f45845f33c32cb7b690959a
chunfeng <chunfengd@xiaohuihui>
2014-06-08 03:07:33 -0400

    python mode

--- .emacs.d/init/20-mode-python.el
+++ .emacs.d/init/20-mode-python.el
@@ -1,11 +1,24 @@
 
-;;(cf-install-packages '(python))
+(cf-install-packages '(python-mode))
+
+(setq abbrev-file-name "~/.emacs.d/abbrev_defs")
+
+(autoload 'python-mode "python-mode" "Python Mode." t)
+ (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+ (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
 (defun cf-init-python ()
   (cf-set-key-bindings
    'local-set-key
    '(
      ("C-c C-c" comment-or-uncomment-region)))
+  (cf-set-key-bindings
+   'define-key
+   '(("C-j" nil)
+     ("C-n" py-newline-and-indent)
+     ("C-M-h" nil)
+     ("C-M-b" py-mark-def-or-class))
+   python-mode-map)
   (setq python-indent-offset 4)
   ;; (c-toggle-auto-state)
   (c-toggle-hungry-state 1))
