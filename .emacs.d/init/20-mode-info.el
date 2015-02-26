e79e33744c1201e612890938187852d984f5c62b
chunfeng <chunfeng@huitu>
2015-02-26 13:42:18 -0500

    info mode key binding

---
+++ .emacs.d/init/20-mode-info.el
@@ -0,0 +1,9 @@
+
+;; info mode
+
+(defun cf-init-info ()
+  (cf-set-key-bindings
+   'local-set-key
+   '(("j" Info-next-reference)
+     ("k" Info-prev-reference))))
+(add-hook 'Info-mode-hook 'cf-init-info)
