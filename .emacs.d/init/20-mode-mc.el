543e66bbc0b9909b3ed63626c2f8e2981e1de7ce
chunfeng <chunfeng@huitu>
2015-03-12 12:15:16 -0400

    set-rectangular-region-anchor

--- .emacs.d/init/20-mode-mc.el
+++ .emacs.d/init/20-mode-mc.el
@@ -9,4 +9,5 @@
    ("C-S-c C-S-c" mc/edit-lines)
    ("C-<" mc/mark-previous-like-this)
    ("C->" mc/mark-next-like-this)
-   ("C-S-c C-<" mc/mark-all-like-this)))
+   ("C-S-c C-<" mc/mark-all-like-this)
+   ("C-S-c C-S-r" set-rectangular-region-anchor)))
