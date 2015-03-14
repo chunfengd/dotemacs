41d9d8b31ecce55b0e7e791f07d0a5ab0aa5b1e3
chunfeng <chunfeng@huitu>
2015-03-14 00:41:45 -0400

    scss key binding

--- .emacs.d/init/20-mode-scss.el
+++ .emacs.d/init/20-mode-scss.el
@@ -1,2 +1,9 @@
 
 (setq scss-compile-at-save nil)
+
+(defun cf-init-scss ()
+  (message "init-scss")
+  (cf-set-key-bindings
+   'local-set-key
+   '(("C-c C-c" ace-jump-mode))))
+(add-hook 'scss-mode-hook 'cf-init-scss)
