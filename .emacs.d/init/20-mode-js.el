6f4f0f28e4c05df946eeb4287e9aadc96b0606aa
chunfeng <chunfeng@huitu>
2015-03-18 00:19:57 -0400

    js

--- .emacs.d/init/20-mode-js.el
+++ .emacs.d/init/20-mode-js.el
@@ -5,8 +5,8 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (defun cf-init-js ()
-  (setq js-indent-level 2)
-  (setq js2-basic-offset 2)
+  (setq-default js-indent-level 2)
+  (setq-default js2-basic-offset 2)
   (cf-set-key-bindings
    'local-set-key
    '(("C-c C-c" ace-jump-mode)
