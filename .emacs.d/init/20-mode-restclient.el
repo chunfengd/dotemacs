94d32dd34c23e94b55cf2c13d3ee682eac3dfce9
chunfeng <chunfeng@huitu>
2015-04-04 00:57:38 -0400

    restclient mode

---
+++ .emacs.d/init/20-mode-restclient.el
@@ -0,0 +1,12 @@
+
+;; (cf-install-packages '(restclient-mode))
+
+(defun cf-init-restclient ()
+  (cf-set-key-bindings
+   'local-set-key
+   '(
+     ("C-c C-c" ace-jump-mode)
+     ("C-c C-h" restclient-http-send-current)
+     ("C-c C-k" restclient-jump-prev)
+     ("C-c C-j" restclient-jump-next))))
+(add-hook 'restclient-mode-hook 'cf-init-restclient)
