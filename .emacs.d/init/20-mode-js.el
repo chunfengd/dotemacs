385a2e992fd382f6456c983e7ac71c0deebd0b2b
chunfeng <chunfeng@huitu>
2015-07-13 12:07:20 -0400

    js mode jslint

--- .emacs.d/init/20-mode-js.el
+++ .emacs.d/init/20-mode-js.el
@@ -4,6 +4,27 @@
 ;; (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
+;; from https://yoo2080.wordpress.com/2012/03/15/js2-mode-setup-recommendation/
+(defun my-add-jslint-declarations ()
+  (when (> (buffer-size) 0)
+    (let ((btext (replace-regexp-in-string
+                  (rx ":" (* " ") "true") " "
+                  (replace-regexp-in-string
+                   (rx (+ (char "\n\t\r "))) " "
+                   ;; only scans first 1000 characters
+                   (save-restriction
+                     (widen)
+                     (buffer-substring-no-properties
+                      (point-min) (min (1+ 1000) (point-max)))) t t))))
+      (mapc (apply-partially 'add-to-list 'js2-additional-externs)
+            (split-string
+             (if (string-match (rx "/*" (* " ") "global" (* " ")
+                                   (group (*? nonl)) (* " ") "*/")
+                               btext)
+                 (match-string-no-properties 1 btext) "")
+             (rx (* " ") "," (* " ")) t)))))
+(add-hook 'js2-post-parse-callbacks 'my-add-jslint-declarations)
+
 (defun cf-init-js ()
   (setq-default js-indent-level 2)
   (setq-default js2-basic-offset 2)
