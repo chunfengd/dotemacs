b4bd57aed796e390384627ca085411848b7ac915
chunfengd <chunfengd@gmail.com>
2016-05-14 11:28:00 -0400

    helm

--- .emacs.d/init/20-mode-helm.el
+++ .emacs.d/init/20-mode-helm.el
@@ -26,19 +26,28 @@
      ("M-y" helm-show-kill-ring)
      ("C-x C-b" helm-mini)
      ("C-x C-f" helm-find-files)
+     ;; ("C-x C-h C-p" helm-projectile)
+     ("C-x C-p" helm-projectile)
+     ("C-x C-d" helm-projectile-find-dir)
+     ("C-x C-m" helm-semantic-or-imenu)
      ("C-x C-h C-o" helm-occur)
+     ("C-x C-h C-p" helm-projectile-switch-project)
+     ("C-x C-h C-r" helm-resume)
+     ("C-x C-h C-m" helm-all-mark-rings)
      ))
 
   ;; general
+  ;; open helm buffer inside current window
   (setq helm-split-window-in-side-p t)
-  (setq helm-move-to-line-cycle-in-source t)
+  ;; (setq helm-move-to-line-cycle-in-source t)
   ;; M-x
   (setq helm-M-x-fuzzy-match t)
   ;; Mini
   (setq helm-buffers-fuzzy-matching t
         helm-recentf-fuzzy-match t)
-  ;; open helm buffer inside current window
-
+  ;; imenu
+  (setq helm-semantic-fuzzy-match t
+        helm-imenu-fuzzy-match    t)
   (setq helm-ff-file-name-history-use-recentf t)
   ;; (helm-autoresize-mode t)
   ;; ;; 40% of current frame height
@@ -62,13 +71,16 @@
   (projectile-global-mode)
   (setq projectile-completion-system 'helm)
   (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
+  ;; (define-key projectile-mode-map (kbd "C-x C-p") 'projectile-command-map)
   (setq projectile-file-exists-remote-cache-expire nil)
+  ;; (setq helm-projectile-fuzzy-match nil)
   (require 'helm-projectile)
   (helm-projectile-on)
   (cf-set-key-bindings
    'define-key
    '(
      ("C-f" projectile-find-file)
-     ("C-h" helm-projectile))
+     ("C-h" helm-projectile)
+     ("C-p" helm-projectile-switch-project))
    projectile-command-map))
 (cf-setup-projectile)
