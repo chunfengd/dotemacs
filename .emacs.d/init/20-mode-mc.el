d9dc27bb6a0c34adbed02034bfb2eadd0cd75e5c
chunfengd <chunfengd@gmail.com>
2016-12-25 19:03:53 -0500

    multi-cusor

--- .emacs.d/init/20-mode-mc.el
+++ .emacs.d/init/20-mode-mc.el
@@ -7,8 +7,8 @@
  'global-set-key
  '(
    ("C-S-c C-S-c" mc/edit-lines)
-   ("C-<" mc/mark-previous-like-this)
-   ("C->" mc/mark-next-like-this)
+   ("C-M-_" mc/mark-previous-like-this)
+   ("C-M-+" mc/mark-next-like-this)
    ("C-S-c C-<" mc/mark-all-like-this)
    ("C-S-c C-S-r" set-rectangular-region-anchor)
    ("C-S-c C-S-k" set-rectangular-region-anchor)))
