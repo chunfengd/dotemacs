5d119e76b46e366c1432d413fb021c9ca28717e0
chunfengd <chunfengd@gmail.com>
2017-02-26 13:45:30 -0500

    remove marmalade & melpa

--- init/04-mode-package.el
+++ init/04-mode-package.el
@@ -6,10 +6,11 @@
 
 (require 'package)
 
-(add-to-list 'package-archives
-             '("marmalade" . "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
+;; (add-to-list 'package-archives
+;;              '("marmalade" . "https://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives
+;;              '("melpa" . "http://melpa.milkbox.net/packages/"))
+
 ;; Org-mode's repository
 (add-to-list 'package-archives
              '("org" . "http://orgmode.org/elpa/") t)
