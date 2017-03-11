c6e2b0c235243515debd62f409716d306b99540c
chunfengd <chunfengd@gmail.com>
2017-03-11 11:49:56 -0500

    update key binding for avy

--- init/20-mode-jump.el
+++ init/20-mode-jump.el
@@ -6,15 +6,15 @@
    'global-set-key
    '(
      ("M-f" nil)
-     ("M-f M-g" avy-goto-char)
-     ("M-f M-2" avy-goto-char-2)
 
      ("M-f M-f" avy-goto-word-1)
-     ("M-f M-j" avy-goto-word-1)
-     ("M-f M-k" avy-goto-char-2)
+     ("M-f M-g" avy-goto-word-0)
+
+     ("M-f M-j" avy-goto-char-2)
+     ("M-f M-k" avy-goto-char)
+
      ("M-f M-l" avy-goto-line)
-     ("M-f M-w" avy-goto-word-0)
-     ))
-  )
+
+     )))
 
 (cf-setup-avy)
