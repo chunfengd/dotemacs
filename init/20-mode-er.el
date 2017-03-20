e2ad6a9d0a2ca084270a87918a76bf25fc475917
chunfengd <chunfengd@gmail.com>
2017-03-19 23:22:23 -0400

    submodule expand-region

--- init/20-mode-er.el
+++ init/20-mode-er.el
@@ -3,8 +3,13 @@
 
 ;; (cf-install-packages '(expand-region))
 
-(cf-set-key-bindings
- 'global-set-key
- '(
-   ("C-=" er/expand-region)
-   ("M-t" er/expand-region)))
+(defun cf-expand-region-setup ()
+  (cf-install-package-file 'expand-region "lib/expand-region/")
+  (cf-set-key-bindings
+   'global-set-key
+   '(
+     ("C-=" er/expand-region)
+     ("M-t" er/expand-region)
+     )))
+
+(cf-expand-region-setup)
