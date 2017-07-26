ac46c43e96291a8fc86fb1498801fa1d37d025ed
chunfengd <chunfengd@gmail.com>
2017-07-25 23:23:22 -0400

    update key binding for info mode

--- init/20-mode-info.el
+++ init/20-mode-info.el
@@ -4,7 +4,10 @@
 (defun cf-init-info ()
   (cf-set-key-bindings
    'local-set-key
-   '(("j" Info-next-reference)
-     ("k" Info-prev-reference))))
-;; (add-hook 'Info-mode-hook 'cf-init-info)
-
+   '(("j" next-line)
+     ("k" previous-line)
+     ("h" backward-char)
+     ("l" forward-char)
+     ("b" Info-history-back)
+     ("?" Info-help))))
+(add-hook 'Info-mode-hook 'cf-init-info)
