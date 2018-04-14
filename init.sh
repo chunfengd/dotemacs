6da4f4d9f2b664192143ff71aee78d72b33af108
chunfengd <chunfengd@gmail.com>
2018-04-13 22:08:18 -0400

    minor

--- init.sh
+++
@@ -1,6 +0,0 @@
-
-# tangle command line
-
-emacs --batch \
-      --load "/Applications/Emacs.app/Contents/Resources/lisp/org/ob-tangle.elc" \
-      --eval "(org-babel-tangle-file \"~/dev/dotemacs/init.org\")"
